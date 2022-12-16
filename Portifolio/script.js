6c45ca9175b7f773b61b0faac7ed14bdec9b7766
jpsoares01 <jpsoares111@hotmail.com>
2022-12-16 11:59:22 -0300

    aprendendo com js

--- Portifolio/script.js
+++ Portifolio/script.js
@@ -1,7 +1,10 @@
-const $html = document.querySelector("html")
-const $checkbox = document.querySelector("#switch")
+const $html = document.querySelector("html");
+const $checkbox = document.querySelector("#switch");
 
+function openDescription(){
+    document.getElementById("descriptionProject").style.display = "block";
+}
 
 $checkbox.addEventListener('change', function(){
-    $html.classList.toggle('dark-mode')
-})
+    $html.classList.toggle('dark-mode');
+});
