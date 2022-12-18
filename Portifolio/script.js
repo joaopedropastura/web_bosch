d589357cde2e0f74b049dccf82094cf269356c97
Joao Pedro Soares <jpsoares111@hotmail.com>
2022-12-17 22:44:44 -0300

    alguem me ajuda?

--- Portifolio/script.js
+++ Portifolio/script.js
@@ -2,7 +2,16 @@ const $html = document.querySelector("html");
 const $checkbox = document.querySelector("#switch");
 
 function openDescription(){
-    document.getElementById("descriptionProject").style.display = "block";
+    let tam = document.getElementById("thisBox");
+    let img = document.getElementById("imgBox");
+    tam.style.transition = "height 1s";
+    tam.style.height = '80vh';
+    img.style.height = 'fit-content';
+        // document.getElementById("descriptionProject").style.display = "block";
+}
+
+function closeDescription(){
+    
 }
 
 $checkbox.addEventListener('change', function(){
