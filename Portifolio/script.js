01d4b9f039e510015279f3820044bb75105e2a5a
Joao Pedro Soares <jpsoares111@hotmail.com>
2022-12-17 23:22:26 -0300

    mds to comecando a odiar isso aqui

--- Portifolio/script.js
+++ Portifolio/script.js
@@ -1,17 +1,24 @@
 const $html = document.querySelector("html");
 const $checkbox = document.querySelector("#switch");
+const btn = document.createElement('button');
 
 function openDescription(){
     let tam = document.getElementById("thisBox");
     let img = document.getElementById("imgBox");
+    let newBtn = document.getElementById('thisBox').appendChild(btn)
+    let oldBtn = document.getElementById('detailsButton')
     tam.style.transition = "height 1s";
     tam.style.height = '80vh';
     img.style.height = 'fit-content';
+    tam.style.alignItems = "initial";
+    newBtn.innerText = 'x';
+    oldBtn.style.display = 'none'
+    newBtn
         // document.getElementById("descriptionProject").style.display = "block";
 }
 
 function closeDescription(){
-    
+
 }
 
 $checkbox.addEventListener('change', function(){
