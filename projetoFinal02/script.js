b77e97be4691e1f4053a79eafb6ba51c66639731
jpsoares01 <jpsoares111@hotmail.com>
2023-01-10 16:45:53 -0300

    chegando mais perto do fim

--- projetoFinal02/script.js
+++ projetoFinal02/script.js
@@ -1,1 +1,24 @@
-$()
+const photo = document.getElementById('userImg');
+const file = document.getElementById('flImage');
+
+
+photo.addEventListener('click', () => {
+    file.click();
+    console.log("OI")
+});
+
+file.addEventListener('change', () => {
+    // Sem essa verificação, ele irá dar erro quando o usuário clicar em cancelar
+    // pois enviará uma "imagem" vazia
+    if (file.files.length == 0) {
+    return;
+    }
+    // Inicializando a função que pega o caminho da imagem
+    let reader = new FileReader();
+    // Está pegando o caminho da imagem
+    reader.readAsDataURL(file.files[0]);
+    // Coloca o caminho da imagem no Source da tag IMG
+    reader.onload = () => {
+    photo.src = reader.result
+    }
+    });
